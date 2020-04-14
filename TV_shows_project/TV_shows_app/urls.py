a5cf60031ca0bfdb7794d01bbfbca3ebdebcb348
Connor <connorskorburg@gmail.com>
2020-04-13 20:11:46 -0500

    updated TV shows project with display pages

--- TV_shows_project/TV_shows_app/urls.py
+++ TV_shows_project/TV_shows_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('shows/new', views.new),
     path('shows/create', views.create),
     path('<int:num1>', views.display),
+    path('shows/', views.shows),
+    path('<int:num1>/edit,', views.edit)
 ]
--- TV_shows_project/TV_shows_app/views.py
+++ TV_shows_project/TV_shows_app/views.py
@@ -10,33 +10,45 @@ def new(request):
     return render(request, 'new.html')
 
 def create(request):
-    # print(request.POST)
-    # print(request.POST['title'])
-    # print(request.POST['network'])
-    # print(request.POST['desc'])
-    # print(request.POST['release_date'])
 
     network = request.POST['network_name']
+    title = request.POST['title']
+    desc = request.POST['desc']
+    release_date = request.POST['release_date']
     all_networks = Network.objects.all()
     
     for net in all_networks:
         if network == net.name:
-            new_network = Network.objects.get(name=network)
-            new_show = Show.objects.create(title=request.POST['title'], network=new_network, desc=request.POST['desc'], release_date=request.POST['release_date'])
+            current_network = Network.objects.get(name=network)
+            new_show = Show.objects.create(title=title, network=current_network, desc=desc, release_date=release_date)
             num1 = int(new_show.id)
             print(num1)
             return redirect(f'/{num1}')
         else:
-            new_network = Network.objects.create(name=request.POST['network_name'])
-            new_show = Show.objects.create(title=request.POST['title'], network=new_network, desc=request.POST['desc'], release_date=request.POST['release_date'])
+            new_network = Network.objects.create(name=network)
+            new_show = Show.objects.create(title=title, network=new_network, desc=desc, release_date=release_date)
             num1 = int(new_show.id)
             print(num1)
-            return redirect(f'{num1}')
+            return redirect(f'/{num1}')
 
 
 def display(request, num1):
+    display_show = Show.objects.get(id=num1)
+
     context = {
         'number': num1,
+        'display_show': display_show,
     }
     
     return render(request, 'display_shows.html', context)
+
+def shows(request):
+    all_shows = Show.objects.all()
+    context = {
+        'all_shows': all_shows,
+    }
+    return render(request, 'shows.html', context)
+
+
+def edit(request):
+    pass
